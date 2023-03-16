2da99de433b5cd30c8597ae4631a21efc1707e90
mmrradif <mmrradif@gmail.com>
2023-03-16 09:05:45 +0600

    Initial Commit

---
+++ HRIS/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HRIS.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string FName { get; set; }
+        public string UName { get; set; }
+    }
+}
---
+++ HRIS/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRIS.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+    }
+}
